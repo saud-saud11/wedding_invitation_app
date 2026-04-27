c45c36e7117322eed6dae45d39c1c1ebf41ece9e
saud-saud11 <substationpit@gmail.com>
2026-04-27 11:47:27 +0300

    feat: integrate Firebase - real auth, firestore, live data

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1363772532"
+    serviceWorkerVersion: "924930764"
   }
 });
